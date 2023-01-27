0dcc176f931e6b45eddd2c67d1b7bae659edc7df
= <juyz0730@gmail.com>
2023-01-27 12:34:07 +0900

    npm setting

---
+++ src/routes.js
@@ -0,0 +1,13 @@
+// const Router = require('@koa/router');
+// const router = new Router();
+// const { myLogging} = require('./middleware/logging');
+// const webController = require('./web/controller')
+// const apiUserController = require('./api/user/controller');
+// const apiFeedController = require('./api/feed/controller');
+// const {verify} = require('./middleware/auth');
+// const multer = require('@koa/multer');
+// const path = require('path');
+// const upload = multer({
+//     dest: path.resolve(__dirname, '../', 'storage')
+// })
+
